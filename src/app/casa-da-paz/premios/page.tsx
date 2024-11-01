1d2cf2656881bf63d73b0282db6fd4a215cb1d9e
Daniel Oliveira <danielmo352@gmail.com>
2024-10-31 21:20:21 -0300

    Card Prêmios
    Card da página Prêmios.

--- src/app/casa-da-paz/premios/page.tsx
+++ src/app/casa-da-paz/premios/page.tsx
@@ -1,18 +1,44 @@
 "use client";
 
-import { Container, Typography, Paper } from '@mui/material';
+import { Grid3x3 } from '@mui/icons-material';
+import { Card, CardContent, Typography, Grid } from '@mui/material';
 
-export default function Premios() {
+const PremiosCertificados = () => {
   return (
-    <Container maxWidth="lg" sx={{ mt: 4 }}>
-      <Paper elevation={3} sx={{ p: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Prêmios
+    <Card sx={{ maxWidth: 345, margin: 2 }}>
+      <CardContent>
+        <Typography variant="h4" component="div">
+          Prêmios e Certificados
         </Typography>
-        <Typography variant="body1" paragraph>
-          Prêmios recebidos para a Instituição Casa da Paz
-        </Typography>
-      </Paper>
-    </Container>
+        <Grid container spacing={6}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="body1" color="text.secondary">
+              Prêmio 1
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Descrição do prêmio 1
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="body1" color="text.secondary">
+              Prêmio 2
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Descrição do prêmio 2
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="body1" color="text.secondary">
+              Prêmio 2
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Descrição do prêmio 2
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
-}
+};
+
+export default PremiosCertificados;
